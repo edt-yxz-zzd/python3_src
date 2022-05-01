8d8c0885b9485ed53ea08c5e841d05994763dd29
edt-yxz-zzd <wuming_zher@zoho.com.cn>
2022-05-01 15:06:14 +0800

    另档hz2513-IDS

---
+++ nn_ns/CJK/CJK_struct/CHISE_IDS_67b94ff_20191211/parse_result___CHISE_IDS.py
@@ -0,0 +1,17 @@
+r'''
+e ../../python3_src/nn_ns/CJK/CJK_struct/CHISE_IDS_67b94ff_20191211/parse_result___CHISE_IDS.py
+nn_ns.CJK.CJK_struct.CHISE_IDS_67b94ff_20191211.parse_result___CHISE_IDS
+from nn_ns.CJK.CJK_struct.CHISE_IDS_67b94ff_20191211.parse_result___CHISE_IDS import hz2tree___BMP_only
+#'''
+
+
+__all__ = '''
+    hz2tree___BMP_only
+    '''.split()
+
+
+
+from nn_ns.CJK.CJK_struct.CHISE_IDS_67b94ff_20191211.load_parse_result___CHISE_IDS import load_hz2tree___BMP_only
+
+hz2tree___BMP_only = load_hz2tree___BMP_only(result_readonly=True)
+
